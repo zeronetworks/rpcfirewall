26c6b23c2c86df3ed4f608a94880ec38ff08ad53
sagiezero <61778471+sagiezero@users.noreply.github.com>
2022-03-13 17:30:34 +0200

    feat(rpcfilter): added uuid and ipv4, along with weights, audit and multiple conditions to the filtering capabilities

--- rpcFwManager/rpcfilters.h
+++ rpcFwManager/rpcfilters.h
@@ -1,4 +1,26 @@
 #pragma once
+#include <optional>
+
+using OpNumFilter = std::optional<DWORD>;
+using UUIDFilter = std::optional<std::wstring>;
+using AddressFilter = std::optional<std::wstring>;
+
+struct RpcCallPolicy
+{
+	bool allow = true;
+	bool audit = false;
+};
+
+struct LineConfig
+{
+	UUIDFilter uuid;
+	OpNumFilter opnum;
+	AddressFilter source_addr;
+	RpcCallPolicy policy;
+	bool verbose;
+};
+
+typedef std::vector<std::pair<std::wstring, LineConfig>> configLinesVector;
 
 void enableAuditingForRPCFilters();
 
@@ -9,3 +31,5 @@ void createIPBlockRPCFilter(std::string);
 void deleteAllRPCFilters();
 
 void installRPCFWProvider();
+
+void createRPCFilterFromTextLines(configLinesVector);
